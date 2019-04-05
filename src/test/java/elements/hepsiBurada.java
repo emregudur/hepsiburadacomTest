c3e07e4a8f303340d118aa7bfd53b8d3fe79f242
Emre Güdür <emre.gudur@gmail.com>
2019-04-05 17:55:26 +0300

    projedeki steplerin çoğu method olarak oluşturuldu

--- src/test/java/elements/hepsiBurada.java
+++ src/test/java/elements/hepsiBurada.java
@@ -4,17 +4,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
+import java.util.List;
 
 public class hepsiBurada {
-    private WebElement homeLoginButtonHover;
-    private WebElement homeLoginButton;
     private driver_ d;
     private WebDriver driver;
+    private WebElement homeLoginButtonHover;//id:myAcount
     private WebElement eposta;//id:email
     private WebElement password;//id:password
     private WebElement loginButton;//class:btn-login-submit
+    private WebElement searchBox;//id:productSearch
+    private WebElement searchPageButton;//class:page-2
+    private WebElement searchPageTwoProduct;//class:page-2
+    // class:keyword
+    private List<WebElement> products;//class:product-detail
+    private WebElement productDetailFavButton;//linktext:Favori Listeme Ekle
 
     public void hepsiBurada() {
         //load page elements
@@ -34,13 +41,16 @@ public class hepsiBurada {
         Assert.assertEquals(str,driver.getTitle());
         System.out.println("Anasayfaya bağlanıldı.");
     }
-    public void goLoginPage() {
+    private void goLoginPage() throws InterruptedException {
         homeLoginButtonHover = driver.findElement(By.id("myAccount"));
-        homeLoginButton = driver.findElement(By.id("login"));
         Actions action = new Actions(driver);
-        action.moveToElement()
+        action.moveToElement(homeLoginButtonHover).perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("login"))).click();
     }
-    public void login() {
+    public void login() throws InterruptedException {
+        goLoginPage();
         eposta = driver.findElement(By.id("email"));
         password = driver.findElement(By.id("password"));
         loginButton= driver.findElement(By.className("btn-login-submit"));
@@ -48,15 +58,48 @@ public class hepsiBurada {
         eposta.sendKeys("emre.gudur@gmail.com");
         password.sendKeys("password123!");
         loginButton.click();
+    }
+    public void seacrh(String searchText){
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("footer-end-wrapper")));
 
         String str="Türkiye'nin En Büyük Online Alışveriş Sitesi Hepsiburada.com";
         Assert.assertEquals(str,driver.getTitle());
         System.out.println("Giriş işlemi başarılı");
-    }
-    public void deneme(){
 
+        searchBox = driver.findElement(By.id("productSearch"));
+        searchBox.sendKeys(searchText);
+        searchBox.submit();
+        goSearchPageTwoAndClickProduct();
     }
-    public void closeDriver(){
+    private void goSearchPageTwoAndClickProduct(){
+        //iphone kelimesi arama kontrolü
+        String str="Türkiye'nin En Büyük Online Alışveriş Sitesi Hepsiburada.com";
+        Assert.assertEquals("\"iphone\"",driver.findElement(By.className("keyword")).getText());
+        System.out.println("Arama işlemi doğru");
+
+        searchPageButton = driver.findElement(By.className("page-2"));
+        searchPageButton.click();
+        Assert.assertEquals("page-2 active",driver.findElement(By.className("page-2")).getAttribute("class"));
+        System.out.println("2. Sayfaya tıklandı.");
+
+        products = driver.findElements(By.className("product-detail"));
+        products.get(2).click();
+
+        productDetailFavButton = driver.findElement(By.linkText("Favori Listeme Ekle"));
+        productDetailFavButton.click();
+        goFavPage();
+    }
+    private void goFavPage(){
+        homeLoginButtonHover = driver.findElement(By.id("myAccount"));
+        Actions action = new Actions(driver);
+        action.moveToElement(homeLoginButtonHover).perform();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("login"))).click();
+    }
+    public void logOut(){
+
         driver.quit();
     }
 }
--- src/test/java/testCase.java
+++ src/test/java/testCase.java
@@ -6,15 +6,18 @@ import org.openqa.selenium.WebDriver;
 public class testCase {
     hepsiBurada hepsiburadacom;
     @Test
-    public void login(){
-        hepsiburadacom= new hepsiBurada();
+    public void login() throws InterruptedException {
+        hepsiburadacom = new hepsiBurada();
         hepsiburadacom.goHome();
+
         hepsiburadacom.login();
+
+        hepsiburadacom.seacrh("iphone");
 
     }
     @After
     public void close() throws InterruptedException {
         Thread.sleep(5000);
-        hepsiburadacom.closeDriver();
+        hepsiburadacom.logOut();
     }
 }
